6944fa4130613edd4187d6daa6e5888be9cc57d2
Tatiana Usanina <tatjana24994@gmail.com>
2020-05-29 11:39:22 +0500

    made changes acording SNTP protocol

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
 import socket
-
+import datetime
+import time
+import struct
 IP = 'localhost'
 PORT = 123
 
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    data = input(">")
-    s.sendto(data.encode(), (IP, PORT))
-    server_data, addr = s.recvfrom(1024)
-    print(bytes.decode(server_data))
+   # FORMAT_DIFF = (datetime.date(1970, 1, 1) - datetime.date(1900, 1, 1)).days * 24 * 3600
+    data = b"\x1b" + 39 * b"\0"
+    transmit_to_send = time.time()
+    data_to_send = data + struct.pack("!II", int(transmit_to_send), int((transmit_to_send - int(transmit_to_send)) * 2 ** 32))
+    len(data_to_send)
+    s.sendto(data_to_send, (IP, PORT))
+    server_data = s.recv(48)
+    time_data = struct.unpack('!B B b b 11I', server_data)
+    orig = time_data[9] + time_data[10] / 2 ** 32
+    recieve_time = time_data[11] + time_data[12] / 2 ** 32
+    transmit = time_data[13] + time_data[14] / 2 ** 32
+    arrive_time = time.time()
+    time_diff = recieve_time - orig - arrive_time + transmit
+    print(datetime.datetime.fromtimestamp(time.time() + time_diff).strftime("%c"))
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
+import datetime
 import socket
+import struct
 import time
 import configparser
-
 def create_configfile(config):
-    config['settings'] = {'sec' : 360 }
+    config['settings'] = {'sec' : 60 }
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
@@ -14,20 +15,37 @@ def read_configfile(config):
 if __name__ == "__main__":
     PORT = 123
     IP = 'localhost'
+
     config = configparser.ConfigParser()
     create_configfile(config)
     sec = int(read_configfile(config))
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((IP, PORT))
-        print("Server is running...")
-        while True:
-            conn, addr = s.recvfrom(1024)
-            data = bytes.decode(conn)
-            if data == 'time':
-                s.sendto(time.strftime("%H : %M : %S , %d %B %Y", time.localtime(time.time() + sec)).encode(), addr)
-            elif data == 'exit':
-                break
-            else:
-                print("Unresolved input")
+        print("Сервер запущен")
+        conn, addr = s.recvfrom(48)
+        receive_time = time.time() + sec
+        print(datetime.datetime.fromtimestamp(receive_time).strftime("%c"))
+        transmit = conn[40:48]
+        orig = transmit
+        mode = 4
+        first_byte_data = struct.unpack("!B", conn[: 1])
+        leap_indicator = first_byte_data[0] >> 6
+        version_num = first_byte_data[0] >> 3 & 0b111
+
+
+        transmit = time.time() + sec
+        send_data = struct.pack("!B", (leap_indicator << 6) + (version_num << 3) + mode) \
+                    + conn[1 : 24] \
+                    + orig \
+                    + struct.pack("!2I",
+                                  int(receive_time), int((receive_time - int(receive_time)) * 2 ** 32)) \
+                    +  struct.pack("!II",
+                               int(transmit), int((transmit - int(transmit)) * 2 ** 32))
+        s.sendto(send_data, addr)
+
+
+
+
+
 
 
